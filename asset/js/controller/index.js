5456d1e5ecd26d0352cae34b552115bfa7cc3b0a
yiyangzhang <yiyangzhang@Ctrip.com>
2015-03-23 18:43:17 +0800

    添加handlebars.js模板引擎

--- asset/js/controller/index.js
+++ asset/js/controller/index.js
@@ -1,6 +1,9 @@
 define(["text!indexHtml"], function(str){
 	function View(){
-		document.getElementById("main").innerHTML = str;
+    /*var data = {"name":"我是标题","content":"首页<br /><a href='#list'>列表页</a>"};
+    var template = Handlebars.compile(str);
+		document.getElementById("main").innerHTML = template(data);*/
+    document.getElementById("main").innerHTML = str;
 	}
 	return View;
 });
