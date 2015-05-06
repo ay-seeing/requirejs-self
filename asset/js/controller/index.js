9da7c3b4224e5f9a8023c90b6d64624817a14d57
ay-seeing <630999015@qq.com>
2015-05-06 20:22:53 +0800

    ..

--- asset/js/controller/index.js
+++ asset/js/controller/index.js
@@ -5,10 +5,24 @@ define([
 ], function(headerHtml, indexHtml, footerHtml) {
   var View = Backbone.View.extend({
     className: "index-page",
+    templateHeader: _.template(headerHtml),
+    templateIndex: _.template(indexHtml),
+    templateFooter: _.template(footerHtml),
     events: {
     	'click .js-link': 'goListAction'
     },
     render: function() {
+      // 获取模板
+      // this.header = _.template($("#header-t").html());
+      // this.footer = _.template($("#footer-t").html());
+      // this.indexPage = _.template($("#index-page").html());
+      // this.listPage = _.template($("#list-page").html());
+
+      // 渲染模板
+      // var headerStr = this.header({message: "hello world!"});
+      // var footerStr = this.footer({message: "hello world!"});
+      // var indexStr = this.indexPage({message: "hello world!"});
+      // var listStr = this.listPage({message: "hello world!"});
     	$("#header").html(headerHtml);
     	$("#footer").html(footerHtml);
       this.$el.html(indexHtml);
